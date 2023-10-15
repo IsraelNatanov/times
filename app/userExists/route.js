03031b89b3d0a746a7ec39f5cea0976ed30006f1
IsraelNatanov <israeln753951@gmail.com>
2023-10-15 17:41:33 +0300

    token

---
+++ app/userExists/route.js
@@ -0,0 +1,17 @@
+
+
+import User from "@/models/user";
+import { connectToDB } from "@/utils/database";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    await connectToDB()
+    const { email } = await req.json();
+    const user = await User.findOne({ email }).select("_id");
+    console.log("user: ", user);
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.log(error);
+  }
+}
